06d8d61e3d395999f2630497e1994fcaef6c517c
Ariel Alves Dutra <alves.lsn@gmail.com>
2019-12-26 16:22:28 -0300

    #30 - Substitui uso de <a> por <Link>

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import {BrowserRouter as Router} from 'react-router-dom'
 
+
+
 import './App.css';
 import Header from './layouts/Header'
 import NavAside from './layouts/NavAside'
 import AppRoutes from './routes'
 
-const App: React.FC = () => {
+
+
+const App: React.FC = () => {  
+  
   return (
     <Router>
       <div className="app">
